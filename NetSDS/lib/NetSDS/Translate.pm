00014421834bc4b8cd835655f8feaa14f876f8ff
Michael Bochkaryov <misha@altlinux.ru>
2009-09-18 18:42:16 +0300

    Fix POD syntax in Translate.pm

--- NetSDS/lib/NetSDS/Translate.pm
+++ NetSDS/lib/NetSDS/Translate.pm
@@ -88,10 +88,6 @@ sub new {
 
 #***********************************************************************
 
-=head1 OBJECT METHODS
-
-=over
-
 =item B<translate($string)> - translate string
 
 Return translated string.
@@ -116,24 +112,35 @@ __END__
 
 =back
 
-=head1 EXAMPLES
-
-
 =head1 BUGS
 
 Unknown yet
 
 =head1 SEE ALSO
 
-None
-
-=head1 TODO
-
-None
+L<Locale::gettext>
 
 =head1 AUTHOR
 
 Michael Bochkaryov <misha@rattler.kiev.ua>
+
+=head1 LICENSE
+
+Copyright (C) 2008-2009 Michael Bochkaryov
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 =cut
 
