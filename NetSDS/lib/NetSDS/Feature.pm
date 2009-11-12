b3b9753ac5926ad23577a159c7b65f5173875f45
Michael Bochkaryov <misha@altlinux.ru>
2009-11-12 14:16:13 +0200

    - [DOC] Licnse added

--- NetSDS/lib/NetSDS/Feature.pm
+++ NetSDS/lib/NetSDS/Feature.pm
@@ -193,22 +193,35 @@ __END__
 
 See C<samples/app_features.pl> script.
 
-=head1 BUGS
-
-Unknown yet
-
 =head1 SEE ALSO
 
-L<NetSDS::App>
+=over
 
-=head1 TODO
+=item * L<NetSDS::App>
 
-None
+=back
 
 =head1 AUTHOR
 
 Michael Bochkaryov <misha@rattler.kiev.ua>
 
-=cut
+=head1 LICENSE
 
+Copyright (C) 2008-2009 Net Style Ltd.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+=cut
 
