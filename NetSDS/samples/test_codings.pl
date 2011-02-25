4cb8af521db917c1b35ee2e94ed0eecc82d1e0e2
Yaroslav Fedevych <jafd@netstyle.com.ua>
2011-02-25 22:29:57 +0200

    Eliminated references to nonexistent modules in test_codings.pl; changed text_* to str_*, so that it can work.

--- NetSDS/samples/test_codings.pl
+++ NetSDS/samples/test_codings.pl
@@ -5,14 +5,13 @@ use strict;
 
 use URI::Escape;
 
-use NetSDS::Util::String qw(text_encode text_decode text_recode);
-use NetSDS::Util::Misc qw(str2uri);
+use NetSDS::Util::String qw(str_encode str_decode str_recode);
 
 my $var = "жопа жопа жопа проверка связи 3-й раз блин не то бегает, что нескол";
 
-$var = text_encode($var);
+$var = str_encode($var);
 
-$var = text_decode($var, "UTF16-BE");
+$var = str_decode($var, "UTF16-BE");
 
 print length($var) . uri_escape($var,"\0-\xff");
 
