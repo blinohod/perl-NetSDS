c87bfe64851312de4c446b00df5131350243e8d9
Yaroslav Fedevych <jafd@netstyle.com.ua>
2011-03-27 16:02:18 +0300

    Refactored base class and JSON mixin so that there is no more NetSDS::Table::Render::JSON; instead, any table can be output as JSON if passed parameter output => 'json' to constructor.

--- NetSDS/lib/NetSDS/Render/Table.pm
+++ NetSDS/lib/NetSDS/Render/Table.pm
@@ -9,6 +9,7 @@ use version;
 use warnings;
 use strict;
 use HTML::Entities;
+use JSON;
 our $VERSION = '1.0000';
 
 use base qw/NetSDS::Class::Abstract/;
@@ -163,6 +164,12 @@ sub format_table_footer {
 
 sub value {
 	my $self = shift;
+	return $self->value_json() if lc($self->params()->{output}) eq 'json';
+	return $self->value_xml();
+}
+
+sub value_xml {
+	my $self = shift;
 	if ( !defined( $self->{__render_state} ) && $self->params()->{body_only} ) {
 		$self->{__render_state} = 'body_start';
 		return '';
@@ -258,6 +265,93 @@ sub isnt_exhausted {
 	return !$self->is_exhausted;
 }
 
+sub json_format_table_start {
+	return '{';
+}
+
+sub json_format_table_end {
+	return '}';
+}
+
+sub json_format_table_header {
+	my  $self = shift;
+	return '"columns" : '.$self->{_JSON}->encode($self->columns_order()).", ";
+}
+
+sub json_body_start {
+	my  $self = shift;
+	return '"data" : [';
+}
+
+sub json_body_end {
+	my  $self = shift;
+	return ']';
+}
+
+sub value_json {
+	my $self = shift;
+	if (!defined($self->{_JSON})) {
+		$self->{_JSON} = JSON->new();
+	}
+	if ( !defined( $self->{__render_state} ) ) {
+		$self->{__render_state} = 'head';
+		return $self->json_format_table_start();
+	} elsif ( $self->{__render_state} eq 'head' ) {
+		$self->{__render_state} = 'body_start';
+		return $self->json_format_table_header();
+	} elsif ( $self->{__render_state} eq 'body_start' ) {
+		$self->{__render_state} = 'body';
+		return $self->json_body_start();
+	} elsif ( $self->{__render_state} eq 'body' ) {
+		if ( $self->dataset->isnt_exhausted ) {
+			my $row = $self->dataset->value();
+			return $self->json_format_table_body_row($row).($self->dataset->isnt_exhausted ? ", " : "");
+		} else {
+			$self->{__render_state} = 'foot';
+			return $self->json_body_end();
+		}
+	} elsif ( $self->{__render_state} eq 'foot' ) {
+		$self->{__render_state} = 'exhausted';
+		return $self->json_format_table_end();
+	}
+} ## end sub value
+
+sub json_format_table_body_row {
+	my ( $self, $row ) = @_;
+	my $cells = $self->json_get_table_row_cells($row);
+	return $self->{_JSON}->encode($cells);
+}
+
+sub json_get_table_row_cells {
+	my ( $self, $row ) = @_;
+	my @cells = ();
+	foreach my $cell ( @{ $self->columns_order } ) {
+		push @cells, $self->json_format_table_body_cell( $row, $cell );
+	}
+	return \@cells;
+}
+
+sub json_format_table_body_cell {
+	my ( $self, $row, $column ) = @_;
+	my $renderer = 'builtin_render_cell_text';
+	my $r        = undef;
+	if ( defined( $self->columns->{$column}->{renderer} ) ) {
+		$r = $self->columns->{$column}->{renderer};
+	} elsif ( defined( $self->column_defaults->{renderer} ) ) {
+		$r = $self->column_defaults->{renderer};
+	}
+	if ($r) {
+		if ( $r =~ /^:/ ) {
+			$r =~ s/^:/builtin_render_cell_/;
+		}
+		$renderer = $r;
+	}
+	my $content = $self->$renderer( $row, $column );
+	return $content;
+}
+
+
+
 sub _hash_to_attributes {
 	my %hash = @_;
 	my @results;
@@ -267,5 +361,6 @@ sub _hash_to_attributes {
 	}
 	return join " ", @results;
 }
+
 
 1;
